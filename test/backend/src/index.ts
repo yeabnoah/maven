d5f7d34f1bb1c2a8bce1a6662b26a49ab35ab253
yeabnoah <yeabnoah5@gmail.com>
2025-01-20 18:52:08 +0300

    added a simple socket and express server

--- test/backend/src/index.ts
+++ test/backend/src/index.ts
@@ -1,1 +1,19 @@
-console.log("the app is working fine");
+import express, { Request, Response } from "express";
+import http from "http";
+import { Server as socketIoServer } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new socketIoServer(server);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(201).json({
+    message: "app running successfully",
+    data: "this app is running hopefully",
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server running in local host");
+});
