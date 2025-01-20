a045d3f1b88c4c82463213d7e4fa9016a7f6e7fb
yeabnoah <yeabnoah5@gmail.com>
2025-01-20 19:53:47 +0300

    added a simple empty vite react project with vs

--- test/backend/src/index.ts
+++ test/backend/src/index.ts
@@ -1,19 +1,21 @@
-import express, { Request, Response } from "express";
-import http from "http";
-import { Server as socketIoServer } from "socket.io";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
 
-const io = new socketIoServer(server);
+const io = new Server(server);
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(201).json({
-    message: "app running successfully",
-    data: "this app is running hopefully",
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("msg", (msg) => {
+    console.log(msg);
+    io.emit("msg", msg);
   });
 });
 
 server.listen(3000, () => {
-  console.log("server running in local host");
+  console.log("Server is running on port 3000");
 });
