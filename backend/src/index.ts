88d3653ec05f878a9fa9c715409192d45751b05f
yeabnoah <yeabnoah5@gmail.com>
2025-01-21 10:13:09 +0300

    added a map to track connected users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import { createServer } from "http";
 import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import express, { Request, Response } from "express";
@@ -6,16 +7,38 @@ import userRoute from "./routes/user.route";
 import { config } from "dotenv";
 import bodyParser from "body-parser";
 import messageRoute from "./routes/message.route";
+import { Server, Socket } from "socket.io";
 
 config();
+let counter = 0;
+const userMap: { [key: string]: string } = {};
+const port = process.env.PORT as string | 3000;
 const app = express();
-const port = 3000;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log(`user connected`);
+
+  const userId = socket.handshake.query.userId as string;
+  const socketId = socket.id;
+  userMap[userId] = socketId;
+
+  counter += 1;
+  console.log(`user amount : ${counter}`);
+  console.log(userMap);
+});
 
 app.use(
   cors({
-    origin: "http://localhost:5000", // Adjust with the correct client-side origin
+    origin: "http://localhost:5000",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true, // Ensure that credentials (cookies, etc.) are sent with requests
+    credentials: true,
   })
 );
 
@@ -41,6 +64,6 @@ app.get("/health", (req: Request, res: Response) => {
   });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Better Auth app listening on port ${port}`);
 });
